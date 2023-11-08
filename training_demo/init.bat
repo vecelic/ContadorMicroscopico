1dfdcb3da6c02f5521cb99eb8b0b311e77dcc3ce
vecelic <30510264+vecelic@users.noreply.github.com>
2023-11-08 10:07:48 -0300

    TF Training

---
+++ training_demo/init.bat
@@ -0,0 +1,6 @@
+python model_main_tf2.py
+  --pipeline_config_path=<F:\Contador\tensorflow\wrkspace\training_demo\models>
+  --model_dir=<F:\Contador\tensorflow\wrkspace\training_demo\models\modelo-microscopia>
+  --checkpoint_every_n=<500>
+  --num_workers=<4>
+  --alsologtostderr
